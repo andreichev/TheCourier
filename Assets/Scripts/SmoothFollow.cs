42f845a18799327a0e37ec35c542fdabd762040f
NGdev1 <andreichev.m@mail.ru>
2019-01-02 00:04:58 +0300

    Unocar suspension configured

---
+++ Assets/Scripts/SmoothFollow.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SmoothFollow : MonoBehaviour
+{
+    public float smoothTime = 0.3f;
+
+    private Transform thisTransform;
+    public Transform target;
+
+    void Start()
+    {
+        thisTransform = transform;
+    }
+    void Update()
+    {
+        thisTransform.LookAt(target);
+    }
+}
