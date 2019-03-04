e88b5fd383c0d4390ba43df685d01e498b0552b3
Misha <andreichev.m@mail.ru>
2019-03-04 12:35:32 +0400

    Camera car follow script added

--- TheCourier/Assets/Scripts/SmoothFollow.cs
+++ TheCourier/Assets/Scripts/SmoothFollow.cs
@@ -3,15 +3,22 @@ using System.Collections;
 
 public class SmoothFollow : MonoBehaviour
 {
-    private Transform thisTransform;
     public Transform target;
+    public Transform lookAtTarget;
+    public float smoothTime = 0.3F;
+
+    private Transform thisTransform;
+    private Vector3 velocity = Vector3.zero;
 
     void Start()
     {
         thisTransform = transform;
     }
-    void Update()
+
+    void FixedUpdate()
     {
-        thisTransform.LookAt(target);
+        // Smoothly move the camera towards that target position
+        thisTransform.position = Vector3.SmoothDamp(thisTransform.position, target.position, ref velocity, smoothTime);
+        thisTransform.LookAt(lookAtTarget);
     }
 }
